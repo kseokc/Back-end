956a0721e14ba4977e766d8a76157b747cefeb35
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-12-19 00:28:04 +0900

    feat : swagger에 토큰 넣는 탭 추가

--- src/main/java/icurriculum/global/config/SwaggerConfig.java
+++ src/main/java/icurriculum/global/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package icurriculum.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +15,25 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         // Define the security scheme
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
 
+        // Define the security requirement
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Authorization");
+
+        // Add the security scheme to components
+        Components components = new Components()
+                .addSecuritySchemes("Authorization", apiKey);
 
         return new OpenAPI()
-                .addServersItem(new Server().url("/swagger"))
+                .addServersItem(new Server().url("/"))
+                .components(components)
+                .addSecurityItem(securityRequirement)
                 .info(apiInfo());
     }
 
